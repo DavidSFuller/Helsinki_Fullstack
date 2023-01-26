69e1e71d644f3c64d6f2d75e6915a655c8a5c67c
DavidSFuller <123096651+DavidSFuller@users.noreply.github.com>
2023-01-26 11:04:53 +0000

    step2

--- Part1/courseinfo/courseinfo/src/App.js
+++ Part1/courseinfo/courseinfo/src/App.js
@@ -5,18 +5,18 @@ return (
   </div>)
 }
 
+const Part = (props) => {
+  return (
+    <p> {props.part} {props.exercises} </p>
+  )
+}
+
 const Content = (props) => {
   return (
     <div>
-      <p>
-        {props.part1} {props.exercise1}
-      </p>
-      <p>
-        {props.part2} {props.exercise2}
-      </p>
-      <p>
-        {props.part3} {props.exercise3}
-      </p>
+      <Part part={props.part1} exercises={props.exercise1} />
+      <Part part={props.part2} exercises={props.exercise2} />
+      <Part part={props.part3} exercises={props.exercise3} />
     </div>
   )
 }
