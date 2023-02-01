439f01f82983fa473324a3c2d677db57421fffc9
DavidSFuller <123096651+DavidSFuller@users.noreply.github.com>
2023-02-01 18:08:06 +0000

    unicafe step4

--- Part1/unicafe/src/App.js
+++ Part1/unicafe/src/App.js
@@ -14,12 +14,37 @@ return (
 )
 }
 
+const Statistics = ({good, bad, all}) => {
+  return (  
+    <div>
+      <b>average {(good-bad) / all}</b>
+      <br/><b>positive {good / all}</b>
+    </div>)
+}
+
+const Results = ({good, neutral, bad, all}) => {
+  if (all === 0) {
+    return(<div><b>No feedback given</b></div>)
+  }
+  return (
+    <div>
+      <Result text="good" amount={good}/>
+      <Result text="neutral" amount={neutral}/>
+      <Result text="bad" amount={bad} all={all}/>
+      <Result text="all" amount={all} all={all}/>
+      <Statistics good={good} bad={bad} all={all}/>
+    </div>
+  )
+}
+
 function App() {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  return (
+  const all = good + neutral + bad
+  console.log(good,neutral,bad,all)
+  
+   return (
     <div>
       <Header text="give feedback"/>
       <button onClick={() => setGood(good + 1)}>good</button>
@@ -27,10 +52,8 @@ function App() {
       <button onClick={() => setBad(bad + 1)}>bad</button>
       <h1>statistics</h1>
       <p/>
-      <Result text="good" amount={good}/>
-      <Result text="neutral" amount={neutral}/>
-      <Result text="bad" amount={bad}/>
-    </div>
+      <Results good={good} neutral={neutral} bad={bad} all={all}/>
+     </div>
   );
 }
 
