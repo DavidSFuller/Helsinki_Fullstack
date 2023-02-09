5bc95bad8bb8e0add144192fc2c6b12fd2d35f7c
DavidSFuller <123096651+DavidSFuller@users.noreply.github.com>
2023-02-09 16:18:13 +0000

    2.9 The Phonebook Step4

--- Part2/phonobook/src/App.js
+++ Part2/phonobook/src/App.js
@@ -4,10 +4,25 @@ const Numbers = ({person}) => <div>{person.name} {person.number}</div>
 
 
 const App = () => {
+
+  // State Variables
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [nameFilter, setNameFilter] = useState('')
+
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas',
-      number: '01287 678191' }
-  ]) 
+      number: '01287 678191'},
+      {name: 'Ada Lovelace',
+      number: '+441913739059'},
+      {name: 'Ros Fuller',
+      number: '+44790598968'}
+    ]) 
+
+  // Event Handlers
+  const handleNameChange = (event) => setNewName(event.target.value)
+  const handleNumberChange = (event) => setNewNumber(event.target.value)
+  const handleFilterChange = (event) => setNameFilter(event.target.value)
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -24,28 +39,25 @@ const App = () => {
     }
   }
 
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
+  // Other functions
+  const namesToShow = (nameFilter === '')
+    ? persons
+    : persons.filter(person => person.name.toLowerCase().includes(nameFilter.toLowerCase()) )
 
-  const handleNameChange = (event) => setNewName(event.target.value)
-  const handleNumberChange = (event) => setNewNumber(event.target.value)
 
+  // ---------- Main body -----------
   return (
     <div>
-      <div>debug: {newName} {newNumber}</div>
+      <div>debug: {newName}-{newNumber}-{nameFilter}</div>
       <h2>Phonebook</h2>
+        filter shown with <input value={nameFilter} onChange={handleFilterChange} />
+      <h2>Add a new entry</h2>
       <form onSubmit={addPerson}>
-      <div>
-          name: <input 
-           value={newName} 
-           onChange={handleNameChange}
-                />
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
-          number: <input 
-           value={newNumber} 
-           onChange={handleNumberChange}
-                />
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit"
@@ -53,9 +65,8 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-        {persons.map(person => <Numbers key={person.name} person={person} />)}
+        {namesToShow.map(person => <Numbers key={person.name} person={person} />)}
     </div>
-
   )
 }
 
