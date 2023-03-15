3eef025d3188b4b6dcb2158d6182e8cf81ac5570
DavidSFuller <123096651+DavidSFuller@users.noreply.github.com>
2023-03-15 14:14:21 +0000

    exercise 4.2

--- Bloglist/index.js
+++ Bloglist/index.js
@@ -1,61 +1,8 @@
-require('dotenv').config()
-const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const morgan = require('morgan')
+const app = require('./src/app') // the actual Express application
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = process.env.MONGODB_URI // was 'mongodb://localhost/bloglist'
-mongoose.connect(mongoUrl)
-
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
-
-app.use(cors())
-app.use(express.json())
-app.use(requestLogger)
-
-morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
-app.use(morgan('tiny'))
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body', {
-  skip: function (req, res) { return req.method !== 'POST' }
-}))
-
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-  console.log('blog:',blog)
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = process.env.PORT // was 3003
+const PORT = config.PORT // was 3003
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  logger.info(`Server running on port ${PORT}`)
 })
---
+++ Bloglist/src/app.js
@@ -0,0 +1,37 @@
+//const http = require('http')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const config = require('../utils/config')
+const blogRouter = require('../controllers/bloglist')
+const middleware = require('../utils/middleware')
+const logger = require('../utils/logger')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+//const Blog = require('../models/bloglist')
+
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger)
+app.use('/api/blogs', blogRouter)
+
+morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
+app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body', {
+  skip: function (req, res) { return req.method !== 'POST' }
+}))
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
